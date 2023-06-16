5564e767be0813afdb22716ea7b9faf9645b6c39
DR0NOV <135983339+DR0NOV@users.noreply.github.com>
2023-06-16 19:12:30 +0300

    Add files via upload

---
+++ num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    for (int i = 0; i <= 5; i++) 
+    {
+        cout << i;
+    }
+    
+    for (int i = 4; i >= 0; i--) 
+    {
+        cout << i;
+    }
+    
+    int q;
+    cin>>q;
+    // почему у меня не работает return 0;
+}
